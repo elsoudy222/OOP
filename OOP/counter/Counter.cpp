435cd90527faf49a574e8bc677eb956ce43ee882
soudyjr <elsoudy222@gmail.com>
2024-05-13 14:43:52 +0400

    operator overloading (OOP)

---
+++ OOP/counter/Counter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Soudy Jr on 13/05/2024.
+//
+
+#include "Counter.h"
---
+++ OOP/counter/Counter.h
@@ -0,0 +1,32 @@
+//
+// Created by Soudy Jr on 13/05/2024.
+//
+
+#ifndef C___COUNTER_H
+#define C___COUNTER_H
+
+
+class Counter {
+private:
+    unsigned int count;
+
+public:
+
+    Counter(): count(0){};
+    Counter(int c): count(c){};
+    int get_count(){
+        return  count;
+    }
+
+    Counter operator ++ (){
+        ++count;
+        return  Counter(count);
+    }
+    Counter operator -- (){
+        ++count;
+        return  Counter(count);
+    }
+};
+
+
+#endif //C___COUNTER_H
